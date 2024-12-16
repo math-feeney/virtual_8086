bcb4fd5a401847b43a6c316a6c2edea74189ca23
Michael Feeney <feeneym225@gmail.com>
2024-12-16 18:10:26 -0500

    added some comments didn't really get anywhere

--- inst_decode.c
+++ inst_decode.c
@@ -1,5 +1,4 @@
 // mov instruction decode for computer enhance
-// testing git commit
 
 #include <stdio.h>
 #include <string.h>
@@ -15,6 +14,13 @@ typedef struct instruct
 int main(int argc, char* argv[])
 {
 
+    // make sure an argument was entered
+    if(argc < 2)
+    {
+        printf("Must enter an input\n");
+        return 1;
+    }
+    
     // get file name from the cmd line arg 
     char file_input[50];
     strcpy(file_input, argv[1]); 
@@ -32,10 +38,12 @@ int main(int argc, char* argv[])
     fptr = fopen(file_input, "r"); 
 
     // read file in
+    // currently just prints one character at a time from the file
+    // specified in the first argument from the cmd line arg
     char file_raw[1000];
     while(fgets(file_raw, 1000, fptr))
     {
-        printf("%s", file_raw);
+        printf("%s", file_raw); // <- CONTINUE HERE I can't figure out how to make print the raw hex values
     }
 
     char test_bin = 0b00001100;
@@ -44,7 +52,7 @@ int main(int argc, char* argv[])
     char test_out = test_bin & 0b00000111;
 
 
-    printf("%x\n", test_out);
+    // printf("%x\n", test_out);
     
     // close file
     fclose(fptr);
